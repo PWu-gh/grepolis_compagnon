2e89db05132a7efb6978d610e63a3a833c129692
Pwu <pwu.dev@outlook.com>
2023-01-14 21:27:53 +0100

    update fto

--- scripts/farmer_town_simplifier.js
+++ scripts/farmer_town_simplifier.js
@@ -6,38 +6,50 @@ document.addEventListener("click", farmer_town_simplifier)
 // auto select toutes les villes de paysans
 // auto accept confirmation
 // auto fermeture de la fenêtre
-
-function farmer_town_simplifier(){
-    let farm_town_window = document.getElementById("fto_town_wrapper");
-
-    if(farm_town_window){
+function farmer_town_simplifier(e){
+    if(e.isTrusted == true){
         setTimeout(() => { // wait 100ms to let the page load
-            console.log("fto opend");
-            let fto_claim_button = document.getElementById("fto_claim_button");
 
-            if(fto_claim_button != null){
-                let claim_btn_is_available = fto_claim_button.classList.contains("disabled") ? false : true;
-                if(claim_btn_is_available){
-                    let select_all_btn = farm_town_window.getElementsByClassName("checkbox select_all")[0];
-                    
-                    if (! select_all_btn.classList.contains("checked")) 
-                        select_all_btn.click();
+            let farm_town_window = document.getElementById("fto_town_wrapper");
+            console.log("tete", farm_town_window);
+
+            if(farm_town_window){
+                let fto_claim_button = document.getElementById("fto_claim_button");
+                console.log("farm opened");
+
+                if(fto_claim_button != null){
+                    let claim_btn_is_available = fto_claim_button.classList.contains("disabled") ? false : true;
+                    if(claim_btn_is_available){
+                        let select_all_btn = farm_town_window.getElementsByClassName("checkbox select_all")[0];
+                        
+                        if (! select_all_btn.classList.contains("checked")) 
+                            select_all_btn.click();
+                    }
+
+                    fto_claim_button.onclick = test()
+                    function test(){
+                        setTimeout(() => { 
+                            document.getElementsByClassName("btn_confirm button_new")[0].click();
+
+        
+                            let fto_window = farm_town_window.parentNode.parentNode.parentNode;
+                            let fto_header = fto_window.getElementsByClassName("ui-dialog-title")[0].parentNode;
+                            let fto_close_btn = fto_header.querySelector('[type="button"][class="icon_right icon_type_speed ui-dialog-titlebar-close"]');
+                            
+                            if(fto_close_btn != undefined)
+                                fto_close_btn.click();
+                        }, 100);
+                    }
                 }
-                fto_claim_button.addEventListener("click", click_fto_confirm);
             }
+        }, 500);
 
-            function click_fto_confirm(){
-                setTimeout(() => { 
-                    document.getElementsByClassName("btn_confirm button_new")[0].click();
-                    fto_claim_button.removeEventListener("click", click_fto_confirm);
-
-                    let fto_window = farm_town_window.parentNode.parentNode.parentNode;
-                    let fto_header = fto_window.getElementsByClassName("ui-dialog-title")[0].parentNode;
-                    let fto_close_btn = fto_header.querySelector('[type="button"][class="icon_right icon_type_speed ui-dialog-titlebar-close"]');
-                    fto_close_btn.click();
-                }, 100);
-            }
-        }, 100);
     }
+
+    
 }
 
+
+function fto_auto_match_time(){
+    
+}
---
+++ scripts/market_gold_exchange.js
@@ -0,0 +1,35 @@
+console.log("start market_gold_exchange")
+document.addEventListener("click", market_simplifier)
+
+
+// simplify the use of farmer town window:
+// auto select toutes les villes de paysans
+// auto accept confirmation
+// auto fermeture de la fenêtre
+function market_simplifier(e){
+    if(e.isTrusted == false){return}
+    
+    setTimeout(() => { // wait 100ms to let the page load
+        let gold_exchange_window = document.getElementById("premium_exchange");
+
+        if(gold_exchange_window){
+            let sell_ress_btn = gold_exchange_window.querySelector('[data-pagenr="1"]')
+            console.log("gold", sell_ress_btn);
+            sell_ress_btn.click()
+        }
+
+        // let fto_claim_button = document.getElementById("premium_exchange");
+
+        // if(fto_claim_button != null){
+        //     let claim_btn_is_available = fto_claim_button.classList.contains("disabled") ? false : true;
+        //     if(claim_btn_is_available){
+        //         let select_all_btn = farm_town_window.getElementsByClassName("gp_tab tab_premium_exchange")[0];
+                
+        //         if (! select_all_btn.classList.contains("checked")) 
+        //             select_all_btn.click();
+        //     }
+        // }
+    }, 200);
+
+    
+}
