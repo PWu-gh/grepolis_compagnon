7598c2c6fac5b84f79902642bc6b61999656ecac
Pwu <pwu.dev@outlook.com>
2023-01-15 04:34:29 +0100

    farm

--- scripts/farmer_town_simplifier.js
+++ scripts/farmer_town_simplifier.js
@@ -7,48 +7,46 @@ document.addEventListener("click", farmer_town_simplifier)
 // auto accept confirmation
 // auto fermeture de la fenêtre
 function farmer_town_simplifier(e){
-    if(e.isTrusted == true){
-        setTimeout(() => { // wait 100ms to let the page load
-            let farm_town_window = document.getElementById("fto_town_wrapper");
+    // if(e.isTrusted == false){return}
+    setTimeout(() => { // wait 100ms to let the page load
+        let farm_town_window = document.getElementById("fto_town_wrapper");
 
-            if(farm_town_window){
-                let fto_claim_button = document.getElementById("fto_claim_button");
-                console.log("farm opened");
+        if(farm_town_window){
+            let fto_claim_button = document.getElementById("fto_claim_button");
 
-                if(fto_claim_button){
-                    let claim_btn_is_available = fto_claim_button.classList.contains("disabled") ? false : true;
-                    if(claim_btn_is_available){
-                        let select_all_btn = farm_town_window.getElementsByClassName("checkbox select_all")[0];
-                        
-                        if (! select_all_btn.classList.contains("checked")) 
-                            select_all_btn.click();
+            if(fto_claim_button){
+                let claim_btn_is_available = fto_claim_button.classList.contains("disabled") ? false : true;
+                if(claim_btn_is_available){
+                    let select_all_btn = farm_town_window.getElementsByClassName("checkbox select_all")[0];
+                    
+                    if (! select_all_btn.classList.contains("checked")){ 
+                        select_all_btn.click();
                     }
 
+                    // fto_claim_button.onclick = ()=>{
+                    //     console.log("halllllllo ???")
+                    // }
                     fto_claim_button.onclick = claim_ressources
-                    function claim_ressources(){
-                        setTimeout(() => { 
-                            document.getElementsByClassName("btn_confirm button_new")[0].click();
-        
-                            let fto_window = farm_town_window.parentNode.parentNode.parentNode;
-                            let fto_header = fto_window.getElementsByClassName("ui-dialog-title")[0].parentNode;
-                            let fto_close_btn = fto_header.querySelector('[type="button"][class="icon_right icon_type_speed ui-dialog-titlebar-close"]');
+                }
 
-                            let cpt = 0
-                            while(true){
-                                cpt ++
-                                console.log("data", cpt);
-                                if(fto_close_btn){
-                                    fto_close_btn.click();
-                                    break
-                                }
-                            }
-                        }, 100);
-                    }
+                
+                function claim_ressources(){
+                    setTimeout(() => { 
+                        let yes_btn = document.getElementsByClassName("btn_confirm button_new")[0]
+                        if (yes_btn)
+                            yes_btn.click();
+                    
+                        let fto_window = farm_town_window.parentNode.parentNode.parentNode;
+                        let fto_header = fto_window.getElementsByClassName("ui-dialog-title")[0].parentNode;
+                        let fto_close_btn = fto_header.querySelector('[type="button"][class="icon_right icon_type_speed ui-dialog-titlebar-close"]');
+                        fto_close_btn.click();
+                    }, 200);
                 }
             }
-        }, 300);
-    }
+        }
+    }, 300);
 }
+
 
 
 function fto_auto_match_time(){
